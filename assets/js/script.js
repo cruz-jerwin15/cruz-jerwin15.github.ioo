51e0f68afe5e7bf2d6414a3ec21f1aa89d204bb4
Jerwin Cruz <cruz.jerwin15@gmail.com>
2023-01-31 14:11:22 +0800

    Completed watch main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,16 @@ toInput.addEventListener('input',()=>{
     }
     toValue.innerText = toInput.value;
 });
+
+const products =[
+    {'id':0,'category':'women','product_name':'Culotte dress','desc':'This is a Cullote dress','img':'dress1.png','price':'$120'},
+    {'id':1,'category':'women','product_name':'Babydoll dress','desc':'This is a Babydoll dress','img':'dress2.jpg','price':'$150'},
+    {'id':2,'category':'women','product_name':'Wrap around dress','desc':'This is a Wrap around dress','img':'dress3.jpg','price':'$150'},
+    {'id':3,'category':'women','product_name':'Kimono dress','desc':'This is a Kimono dress','img':'dress4.jpg','price':'$150'},
+    {'id':4,'category':'women','product_name':'Bubble dress','desc':'This is Bubble dress','img':'dress5.jpg','price':'$150'},
+    {'id':5,'category':'women','product_name':'Tutu dress','desc':'This is a Tutu dress','img':'dress6.jpg','price':'$150'},
+    {'id':6,'category':'women','product_name':'Polo dress','desc':'This is a Polo dress','img':'dress7.jpg','price':'$150'},
+    {'id':7,'category':'women','product_name':'Shirtdress','desc':'This is a Shirtdress','img':'dress9.jpg','price':'$150'},
+    {'id':8,'category':'women','product_name':'Sundress','desc':'This is a Sundress','img':'dress8.jpg','price':'$150'}
+];
+
